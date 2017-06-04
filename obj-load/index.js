84192f015501c48b9f072b62a165dc0ad273414a
Bacher <bacherko@gmail.com>
2017-06-04 16:21:01 +0300

    tmp

--- obj-load/index.js
+++ obj-load/index.js
@@ -1,8 +1,9 @@
 const tank = parseObj(TANK_OBJ);
-const turret2 = tank.groups['Turret_2'];
 
 const mView = mat4.create();
 const mMove = mat4.create();
+
+const textures = {};
 
 let gl;
 let shaderProgram;
@@ -12,25 +13,47 @@ uniform mat4 umView;
 uniform mat4 umMove;
 
 attribute vec3 aPos;
-//attribute vec4 aColor;
+attribute vec3 aN;
+attribute vec2 aUV;
 
-//varying vec4 vColor;
+//varying vec3 vNormal;
+varying vec2 vUV;
 
 void main(void) {
     gl_Position = umView * umMove * vec4(aPos, 1.0);
-    //vColor = aColor;
+    vUV = aUV;
+    //vNormal = aN;
 }
 `;
 
 const fragmentShaderText = `
 precision mediump float;
 
-//varying vec4 vColor;
+uniform sampler2D uSampler;
+
+//varying vec3 vNormal;
+varying vec2 vUV;
 
 void main(void) {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    //gl_FragColor = texture2D(uSampler, vec2(vNormal.x, vNormal.y));
+    gl_FragColor = texture2D(uSampler, vUV);
 }
 `;
+
+function initShaderLocations() {
+    shaderProgram.umView   = gl.getUniformLocation(shaderProgram, 'umView');
+    shaderProgram.umMove   = gl.getUniformLocation(shaderProgram, 'umMove');
+    shaderProgram.uSampler = gl.getUniformLocation(shaderProgram, 'uSampler');
+
+    shaderProgram.aPos = gl.getAttribLocation(shaderProgram, 'aPos');
+    gl.enableVertexAttribArray(shaderProgram.aPos);
+
+    shaderProgram.aN = gl.getAttribLocation(shaderProgram, 'aN');
+    gl.enableVertexAttribArray(shaderProgram.aN);
+
+    shaderProgram.aUV = gl.getAttribLocation(shaderProgram, 'aUV');
+    gl.enableVertexAttribArray(shaderProgram.aUV);
+}
 
 function setMatrixUniforms() {
     gl.uniformMatrix4fv(shaderProgram.umView, false, mView);
@@ -42,12 +65,38 @@ function initBuffers() {
     gl.bindBuffer(gl.ARRAY_BUFFER, tank.bufVertices);
     gl.bufferData(gl.ARRAY_BUFFER, tank.vertices, gl.STATIC_DRAW);
 
+    tank.bufNormals = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, tank.bufNormals);
+    gl.bufferData(gl.ARRAY_BUFFER, tank.normals, gl.STATIC_DRAW);
+
+    tank.bufUVs = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, tank.bufUVs);
+    gl.bufferData(gl.ARRAY_BUFFER, tank.uvs, gl.STATIC_DRAW);
+
     const indexArray = [];
 
     for (let polygon of tank.polygons) {
         for (let point of polygon) {
             indexArray.push(point.vertex);
         }
+
+        // for (let point of polygon) {
+        //     indexArray.push(point.uv);
+        // }
+        //
+        // for (let point of polygon) {
+        //     indexArray.push(point.normal);
+        // }
+
+        // for (let point of polygon) {
+        //     if (
+        //         typeof point.vertex !== 'number' ||
+        //         typeof point.uv !== 'number' ||
+        //         typeof point.normal !== 'number'
+        //     ) {
+        //         debugger
+        //     }
+        // }
     }
 
     const uIndexArray = new Uint16Array(indexArray);
@@ -56,10 +105,12 @@ function initBuffers() {
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, tank.bufIndex);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, uIndexArray, gl.STATIC_DRAW);
     tank.bufIndexLength = uIndexArray.length;
+}
 
-    // turret2.bufColors = gl.createBuffer();
-    // gl.bindBuffer(gl.ARRAY_BUFFER, triangle.bufColors);
-    // gl.bufferData(gl.ARRAY_BUFFER, triangle.colors, gl.STATIC_DRAW);
+function initTextures() {
+    for (let group of tank.groups) {
+        textures[group.material] = createTexture(group.material);
+    }
 }
 
 let rotateAngle = 0;
@@ -75,17 +126,26 @@ function drawScene() {
     mat4.identity(mMove);
     gl.bindBuffer(gl.ARRAY_BUFFER, tank.bufVertices);
     gl.vertexAttribPointer(shaderProgram.aPos, 3, gl.FLOAT, false, 0, 0);
-    // gl.bindBuffer(gl.ARRAY_BUFFER, triangle.bufColors);
-    // gl.vertexAttribPointer(shaderProgram.aColor, 4, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, tank.bufNormals);
+    gl.vertexAttribPointer(shaderProgram.aN, 3, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, tank.bufUVs);
+    gl.vertexAttribPointer(shaderProgram.aUV, 2, gl.FLOAT, false, 0, 0);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.uniform1i(shaderProgram.uSampler, 0);
+
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, tank.bufIndex);
     setMatrixUniforms();
 
-    for (let groupName in tank.groups) {
-        const group = tank.groups[groupName];
-
-        if (groupName !== 'Turret_2') {
+    for (let group of tank.groups) {
+        if (group.id !== 'Turret_2') {
             //continue;
         }
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, textures[group.material]);
 
         //console.log(group, tank.bufIndexLength);
         gl.drawElements(gl.TRIANGLES, group.size * 3, gl.UNSIGNED_SHORT, group.offset * 6);
@@ -97,16 +157,9 @@ function drawScene() {
 
 initGL(document.getElementById('canvas'));
 shaderProgram = initShaderProgram();
-
-shaderProgram.umView = gl.getUniformLocation(shaderProgram, 'umView');
-shaderProgram.umMove = gl.getUniformLocation(shaderProgram, 'umMove');
-shaderProgram.aPos   = gl.getAttribLocation(shaderProgram, 'aPos');
-//shaderProgram.aColor = gl.getAttribLocation(shaderProgram, 'aColor');
-
-gl.enableVertexAttribArray(shaderProgram.aPos);
-//gl.enableVertexAttribArray(shaderProgram.aColor);
-
+initShaderLocations();
 initBuffers();
+initTextures();
 
 gl.clearColor(0, 0, 0, 1);
 gl.enable(gl.DEPTH_TEST);
@@ -125,4 +178,4 @@ function tick(newTime) {
     requestAnimationFrame(tick);
 }
 
-tick();
+setTimeout(tick, 1000);
